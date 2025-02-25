caf33a8f7f50b83d7dcbd281ff6355c31de6db3d
ibrandiaye <ibra789ndiaye@gmail.com>
2025-02-25 15:59:25 +0000

    dd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -156,7 +156,7 @@
                 <div class="card-header  text-center">LISTE D'ENREGISTREMENT DES Comptages</div>
                     <div class="card-body">
                     
-                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                        <table id="datatable-" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                             <thead>
                                 <tr>
                                     <th>Departement</th>
